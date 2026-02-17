319195e3c3977da287b0bc7d5570b02c6fa642be
Boxur <boxur@boxur.org>
2026-02-17 22:42:15 +0100

    small cleanup

--- MoveGenerator.h
+++ MoveGenerator.h
@@ -5,10 +5,10 @@
 #include "Move.h"
 #include <vector>
 
-const uint64_t FILE_A = 0x0101010101010101ULL;
-const uint64_t FILE_B = 0x0202020202020202ULL;
-const uint64_t FILE_G = 0x4040404040404040ULL;
-const uint64_t FILE_H = 0x8080808080808080ULL;
+constexpr uint64_t FILE_A = 0x0101010101010101ULL;
+constexpr uint64_t FILE_B = 0x0202020202020202ULL;
+constexpr uint64_t FILE_G = 0x4040404040404040ULL;
+constexpr uint64_t FILE_H = 0x8080808080808080ULL;
 
 enum MoveFlags {
     NO_FLAG = 0,
@@ -21,7 +21,7 @@ enum MoveFlags {
     PROM_KNIGHT = 7
 };
 
-#define BOARD_SIZE 64
+constexpr int BOARD_SIZE = 64;
 
 void initAll();
 
